7a4604fa118ec7ef17d484b52f86d48cff65c012
facunava92 <facunava92@gmail.com>
2019-08-24 03:46:38 -0300

    Agrego TP5 de tecnicas, hecho hasta el 2 inclusive

---
+++ Tecnicas Digitales 3/TP/TPN5 - Introducción al dsPIC/dspic1.X/main.c
@@ -0,0 +1,62 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+/* Device header file */
+#if defined(__XC16__)
+    #include <xc.h>
+#elif defined(__C30__)
+    #if defined(__dsPIC33E__)
+    	#include <p33Exxxx.h>
+    #elif defined(__dsPIC33F__)
+    	#include <p33Fxxxx.h>
+    #endif
+#endif
+
+
+#include <stdint.h>        /* Includes uint16_t definition                    */
+#include <stdbool.h>       /* Includes true/false definition                  */
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp              */
+
+/******************************************************************************/
+/* Global Variable Declaration                                                */
+/******************************************************************************/
+
+/* i.e. uint16_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+
+void main(void)
+{
+
+    /* Configure the oscillator for the device */
+    ConfigureOscillator();
+
+    /* Initialize IO ports and peripherals */
+    InitApp();
+
+    /* TODO <INSERT USER APPLICATION CODE HERE> */
+   // uint32_t i;
+
+    
+    while(1)
+    {
+/*
+ // #### 1.1
+    _LATB2 = _RB7;
+    _LATB3 = _RB10;
+ */
+    
+    /*
+    uint32_t i;
+    for(i=0; i<=155000; i++);
+    _LATB2 = ~_LATB2;
+    _LATB3 = ~_LATB3;
+    */
+        
+    }
+}
---
+++ Tecnicas Digitales 3/TP/TPN5 - Introducción al dsPIC/dspic1.X/user.h
@@ -0,0 +1,16 @@
+/******************************************************************************/
+/* User Level #define Macros                                                  */
+/******************************************************************************/
+
+/* TODO Application specific user parameters used in user.c may go here */
+
+/******************************************************************************/
+/* User Function Prototypes                                                   */
+/******************************************************************************/
+
+/* TODO User level functions prototypes (i.e. InitApp) go here */
+
+void InitApp(void);         /* I/O and Peripheral Initialization */
+void LED(void);
+void Timer(void);
+void PLL(void);
